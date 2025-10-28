ef2828e154ba526aaf936cbabecd7f7476d0bd0c
BashukOleksii <oleksiichort@gmail.com>
2025-10-28 19:01:39 +0200

    Готово GET

---
+++ SubscriptionManager/SubscriptionManager.API/Controllers/SubController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SubscriptionManager.API.IServices;
+using SubscriptionManager.Core.Models;
+
+namespace SubscriptionManager.API.Controllers
+{
+    [Route("api/Subscriptions")]
+    [ApiController]
+    public class SubController : ControllerBase
+    {
+        private readonly ISubService _subService;
+
+        public SubController(ISubService subservice)
+        {
+            _subService = subservice;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SubscriptionItem>>> GetAll()
+        {
+            try
+            {
+                var subs = await _subService.GetAllAsync();
+
+                return Ok(subs);
+            }
+            catch (KeyNotFoundException kex)
+            {
+                return NotFound(kex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SubscriptionItem>> GetById(string id)
+        {
+            try
+            {
+                var sub = await _subService.GetByIdAsync(id);
+                return Ok(sub);
+            }
+            catch(KeyNotFoundException kex)
+            {
+                return NotFound(kex.Message);
+            }
+
+        }
+    }
+}
